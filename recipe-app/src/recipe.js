8674435430ff742033dc2a6ea273e5dead79d49c
Catsudemo <cattmott@gmail.com>
2019-10-24 15:02:05 +0200

    Week 1 homework
    Searchable recipe app

---
+++ recipe-app/src/recipe.js
@@ -0,0 +1,30 @@
+import React from 'react';
+const Recipe = ({ title, image, ingredients, url, source, labels }) => {
+  return (
+    <div className="recipe">
+      <div className="recipe-item-wrapper">
+        <div className="recipe-item-container">
+          <img className={`image+${title}`} src={image} alt=""></img>
+          <div className="recipe-item-content">
+            <h3 className={title}>{title}</h3>
+            <span className="recipe-source">{source}</span>
+            <ol className={`ingredients+${title}`}>
+              <p>
+                <b>Ingredients:</b>
+              </p>
+              {ingredients.map(ingredient => (
+                <li>{ingredient.text}</li>
+              ))}
+            </ol>
+            <a href={url} className="recipeLink" target="_blank" rel="noopener noreferrer">
+              Go to Recipe
+            </a>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recipe;
